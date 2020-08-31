309fb82c82a6899eefba0f484b84813c68642b55
wangjs5 <wangjs5@ifeng.com>
2020-08-31 15:25:24 +0800

    first comment

---
+++ src/com/laughitover/linkedList/LinkedReverse.java
@@ -0,0 +1,81 @@
+package com.laughitover.linkedList;
+
+
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+public class LinkedReverse {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Solution solution=new Solution();
+        ListNode head=new ListNode(1);
+        solution.reverseList(head);
+    }
+}
+
+/**
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
